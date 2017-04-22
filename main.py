56c7805396f9febb3a52517b5d40a9e43daf375e
Jake Crawford <jake.crawford@outlook.com>
2017-04-22 15:24:01 -0600

    Add hello world endpoint for arduino

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 from app import app
+from flask import request
+
+@app.route('/input', methods = ['POST'])
+def input():
+	# This is the function that will first be handed the arduino data
+	print(request.data)
 
 if __name__ == '__main__':
     app.run(debug=True)
