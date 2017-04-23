3f1aa28a85196e2bb752aade9f60415d43dc80cd
Jake Crawford <jake.crawford@outlook.com>
2017-04-22 20:43:07 -0600

    Add input into DB procedure

--- app/main.py
+++ app/main.py
@@ -1,14 +1,36 @@
 from app import app
 from flask import request
+import sqlite3 as sql
 
 
-@app.route('/input', methods = ['POST'])
+@app.route('/input', methods = ['GET'])
 def input():
 	# get query vars
+	fuellevel = request.args.get('fuellevel')
+	errno = request.args.get('errno')
+	speed = request.args.get('speed')
+	fuelrate = request.args.get('fuelrate')
+	enginetemp = request.args.get('enginetemp')
 
-	# process query vars
+
+	# process query vars for insertion
+	speed = speed*.621371
+	fuelrate = (1/(fuelrate*.26417217685))*speed
+	enginetemp = enginetemp*(9/5) + 32
+
+	dbEntry = (fuellevel, errno, speed, fuelrate, enginetemp)
+	
+	#db
+	conn = sql.connect('obdbuddy.db')
+	cur = conn.cursor()
+	cur.execute("INSERT INTO vehicle_data (fuellevel, errno, speed, fuelrate, enginetemp) VALUES (?,?,?,?,?)",
+		dbEntry)
+	conn.commit()
+	conn.close()
 
 	
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
