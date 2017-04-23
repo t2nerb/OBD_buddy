2fef5b09c0d1f62888c58cd684a0a6c6815eb602
t2nerb <t2nerb@gmail.com>
2017-04-23 00:38:23 -0600

    Found the missing semi-colon

--- app/main.py
+++ app/main.py
@@ -4,6 +4,11 @@ import sqlite3 as sql
 
 my_timestamp = 0
 
+@app.route('/', methods  = ['GET'])
+def index():
+    print('EXAMPLE')
+    return 'fuck yah'
+
 @app.route('/input', methods = ['GET'])
 def input():
     # get query vars
@@ -13,22 +18,24 @@ def input():
     fuelrate = request.args.get('fuelrate')
     enginetemp = request.args.get('enginetemp')
 
-
+    """
     # process query vars for insertion
     speed = speed*.621371
     fuelrate = (1/(fuelrate*.26417217685))*speed
     enginetemp = enginetemp*(9/5) + 32
+    """
 
-    dbEntry = (fuellevel, errno, speed, fuelrate, enginetemp)
+    dbEntry = (fuellevel, errno, speed, fuelrate, enginetemp) 
     
     #db
     conn = sql.connect('obdbuddy.db')
     cur = conn.cursor()
-    cur.execute("INSERT INTO vehicle_data (fuellevel, errno, speed, fuelrate, enginetemp) VALUES (?,?,?,?,?)",
+    cur.execute("INSERT INTO vehicle_data (fuellevel, errno, speed, fuelrate, enginetemp) VALUES (?,?,?,?,?);",
             dbEntry)
     conn.commit()
     conn.close()
-    return
+
+    return 'yas'
 
 @app.route('/receive', methods = ['GET'])
 def output():
@@ -41,14 +48,17 @@ def output():
     return_data = []
     my_timestamp = 0;
 
-    conn = sql.connect('odbbuddy.db')
+    conn = sql.connect('obdbuddy.db')
 
     # query database for all times greater than my_timestamp
     for row in conn.execute('SELECT * FROM vehicle_data WHERE timestamp > ', \
             my_timestamp, ' ORDER BY timestamp ASC'):
+        print(row)
+        """
         return_element['fuel'] = row['fuel']
         return_element['speed'] = row['speed']
         return_element['fuelrate'] = row['fuelrate']
+        """
 
         my_timestamp = row['timestamp']
         return_data.append(return_element)
