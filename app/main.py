d7d9a5db015360d9c8986a6fe34ac5a2c2739da9
Jake Crawford <jake.crawford@outlook.com>
2017-04-22 20:18:08 -0600

    Add db

--- app/main.py
+++ app/main.py
@@ -1,10 +1,14 @@
 from app import app
 from flask import request
 
+
 @app.route('/input', methods = ['POST'])
 def input():
-	# This is the function that will first be handed the arduino data
-	print(request.data)
+	# get query vars
+
+	# process query vars
+
+	
 
 if __name__ == '__main__':
     app.run(debug=True)
